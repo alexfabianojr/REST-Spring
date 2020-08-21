256057351a12044f814c33a44e751d95b9b4311e
alexfabianojr <alexfabianojr@gmail.com>
2020-08-21 02:24:21 -0300

    safe commit

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,7 +1,6 @@
 package br.com.alura.forum.controller;
 
 import java.net.URI;
-import java.util.List;
 import java.util.Optional;
 
 import br.com.alura.forum.controller.dto.DetalhesDoTopicoDto;
@@ -9,6 +8,12 @@ import br.com.alura.forum.controller.form.AtualizacaoTopicoForm;
 import br.com.alura.forum.controller.form.TopicoForm;
 import br.com.alura.forum.repository.CursoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
@@ -31,18 +36,24 @@ public class TopicosController {
 	private CursoRepository cursoRepository;
 
 	//Dto(s) são imporantes para não ficar retornando a entidade da JPA (por segurança)
+	//http://localhost:8080/topicos?page=0&size=10&sort=id,asc   ou ,desc
+	@Cacheable(value = "listaDeTopicos")
 	@GetMapping
-	public List<TopicoDto> lista(String nomeCurso) {
-		List<Topico> topicos;
+	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso,
+								 @PageableDefault(sort = "id", direction = Sort.Direction.DESC, page = 0, size = 10)
+										 Pageable paginacao) {
+		Page<Topico> topicos;
 		if (nomeCurso == null) {
-			topicos = topicoRepository.findAll();
+			topicos = topicoRepository.findAll(paginacao);
 		} else {
-			topicos = topicoRepository.findByCursoNome(nomeCurso);
+			topicos = topicoRepository.findByCursoNome(nomeCurso, paginacao);
 		}
 		return TopicoDto.converter(topicos);
 	}
 
 	@PostMapping
+	@Transactional
+	@CacheEvict(value = "listaDeTopicos", allEntries = true) //limpa o cache
 	public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder builder) {
 		Topico topico = form.converter(cursoRepository);
 		topicoRepository.save(topico);
@@ -61,6 +72,7 @@ public class TopicosController {
 
 	@PutMapping("/{id}")
 	@Transactional //Ao finalizar o método, o Spring efetuará o commit automático da transação, caso nenhuma exception tenha sido lançada
+	@CacheEvict(value = "listaDeTopicos", allEntries = true)
 	public ResponseEntity<TopicoDto> atualizar(@PathVariable("id") Long id,@RequestBody @Valid AtualizacaoTopicoForm atualizaForm) {
 		Optional<Topico> topico = topicoRepository.findById(id);
 		return topico.map(value -> ResponseEntity.ok(new TopicoDto(topico.get())))
@@ -69,6 +81,7 @@ public class TopicosController {
 
 	@DeleteMapping("/{id}")
 	@Transactional
+	@CacheEvict(value = "listaDeTopicos", allEntries = true)
 	public ResponseEntity<?> remover(@PathVariable Long id) {
 		Optional<Topico> topico = topicoRepository.findById(id);
 		if (topico.isPresent()) {
