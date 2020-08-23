06e41d5397fffe6e4da423aef16f179aa46d9ab9
alexfabianojr <alexfabianojr@gmail.com>
2020-08-22 21:30:34 -0300

    "token"

---
+++ src/main/java/br/com/alura/forum/config/security/AutenticacaoService.java
@@ -0,0 +1,25 @@
+package br.com.alura.forum.config.security;
+
+import br.com.alura.forum.modelo.Usuario;
+import br.com.alura.forum.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AutenticacaoService implements UserDetailsService {
+
+    @Autowired
+    UsuarioRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<Usuario> byEmail = repository.findByEmail(username);
+        if (byEmail.isPresent()) return byEmail.get();
+        throw new UsernameNotFoundException("Dados Invalidos");
+    }
+}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$2hrKxXiTIKnUHsNwfr8lzuWGKyD4ZSASlOwucDFmeOa9C0i2ndzaa');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
